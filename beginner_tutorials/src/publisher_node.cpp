0e9f074400b120696b316ef11da8f4519f2e3687
sarangnair1998 <sarangshibu98@gmail.com>
2024-11-15 12:54:22 -0500

    Seperated the test files

--- beginner_tutorials/src/publisher_node.cpp
+++ beginner_tutorials/src/publisher_node.cpp
@@ -1,27 +1,13 @@
-// Copyright 2024 Sarang
-
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
-#include <std_srvs/srv/set_bool.hpp>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/msg/transform_stamped.hpp>
+#include "beginner_tutorials/srv/toggle_publishing_service.hpp"
 
 using namespace std::chrono_literals;
 
-/** @file
- *  @brief PublisherNode publishes messages and provides a service to toggle the publishing on or off.
- */
-
-/**
- * @class PublisherNode
- * @brief A ROS2 node that publishes messages periodically, broadcasts a TF transform, and provides a service to toggle publishing on or off.
- */
 class PublisherNode : public rclcpp::Node {
  public:
-    /**
-     * @brief Constructor for PublisherNode.
-     * Initializes the publisher, timer, service for toggling publishing, and the TF broadcaster.
-     */
     PublisherNode()
         : Node("publisher_node"), message_("Hello, this is Sarang"),
           publishing_enabled_(true) {
@@ -38,8 +24,7 @@ class PublisherNode : public rclcpp::Node {
             std::bind(&PublisherNode::publish_message, this));
 
         // Create a service to toggle publishing on/off
-        toggle_publishing_service_ = this->create_service<
-        std_srvs::srv::SetBool>(
+        toggle_publishing_service_ = this->create_service<beginner_tutorials::srv::TogglePublishingService>(
             "toggle_publishing",
             std::bind(&PublisherNode::handle_toggle_publishing, this,
                       std::placeholders::_1, std::placeholders::_2));
@@ -57,10 +42,6 @@ class PublisherNode : public rclcpp::Node {
     }
 
  private:
-    /**
-     * @brief Publishes a message if publishing is enabled.
-     * Checks if publishing is enabled before publishing a message to the topic.
-     */
     void publish_message() {
         if (!publishing_enabled_) {
             RCLCPP_DEBUG_STREAM(this->get_logger(),
@@ -74,16 +55,9 @@ class PublisherNode : public rclcpp::Node {
         publisher_->publish(message);
     }
 
-    /**
-     * @brief Handles the toggle publishing service request.
-     * @param request The request containing the desired publishing state
-     * (true to enable, false to disable).
-     * @param response The response indicating whether the request was successful
-     * and a message describing the result.
-     */
     void handle_toggle_publishing(
-        const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
-        std::shared_ptr<std_srvs::srv::SetBool::Response> response) {
+        const std::shared_ptr<beginner_tutorials::srv::TogglePublishingService::Request> request,
+        std::shared_ptr<beginner_tutorials::srv::TogglePublishingService::Response> response) {
         // Update the publishing state based on the service request
         publishing_enabled_ = request->data;
 
@@ -100,12 +74,8 @@ class PublisherNode : public rclcpp::Node {
         response->success = true;
     }
 
-    /**
-     * @brief Broadcasts a TF transform with the frame "talk" as child of "world".
-     */
     void broadcast_tf() {
         geometry_msgs::msg::TransformStamped transformStamped;
-        // Set the header and frame information
         transformStamped.header.stamp = this->now();
         transformStamped.header.frame_id = "world";
         transformStamped.child_frame_id = "talk";
@@ -121,26 +91,18 @@ class PublisherNode : public rclcpp::Node {
         transformStamped.transform.rotation.z = 0.707;  // 90 degrees
         transformStamped.transform.rotation.w = 0.707;
 
-        // Broadcast the transform
         tf_broadcaster_->sendTransform(transformStamped);
     }
 
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr
-        toggle_publishing_service_;
+    rclcpp::Service<beginner_tutorials::srv::TogglePublishingService>::SharedPtr toggle_publishing_service_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::TimerBase::SharedPtr tf_timer_;
     std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-    std::string message_; /**< The message to be published. */
+    std::string message_; 
     bool publishing_enabled_;
 };
 
-/**
- * @brief Main function to initialize and spin the PublisherNode.
- * @param argc Argument count.
- * @param argv Argument values.
- * @return int Exit status code.
- */
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<PublisherNode>();
